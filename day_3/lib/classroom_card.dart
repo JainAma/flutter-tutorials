1f5bce663f193c3193d4c1aacf0cfa1fdcc7f145
meditat <shivam.singh.calif@gmail.com>
2020-01-11 17:34:16 +0530

    Day 3 Flutter Code

---
+++ day_3/lib/classroom_card.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ClassroomCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Stack(
+      children: <Widget>[
+        Image.asset('assets/images/dog.jpg'),
+        ListTile(
+          title: Text("Data Structure"),
+          subtitle: Text("Stacks,LinkedList, Hash"),
+          trailing: CircleAvatar(),
+        )
+      ],
+    ));
+  }
+}
